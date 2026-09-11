bf26c6f80c9024cd4c8eb12be7db884cc60398a4
jordan tshitshi <jordantshitshi@icloud.com>
2026-09-11 20:51:30 +0100

    Configurer les proxies HTTPS pour Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(
+        at: '*',
+    );
+
     $middleware->alias([
         'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
     ]);
-    })
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
